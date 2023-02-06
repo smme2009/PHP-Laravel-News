93f9f9f1dcc561cac8892e87483c5e7e5b6fe9d5
smme2009 <smme2009@gmail.com>
2023-02-06 15:13:01 +0800

    調整Model時間欄位使用UNIX Timestamp輸出

--- app/Kernel/model.php
+++ app/Kernel/model.php
@@ -10,11 +10,18 @@ class Model extends LaravelModel
     //啟用軟刪除
     use SoftDeletes;
 
-    //時間戳使用UNIX Timestamp 
+    //時間欄位使用UNIX Timestamp儲存 
     protected $dateFormat = 'U';
 
-    //調整時間戳相關欄位名稱
+    //調整時間欄位名稱
     const CREATED_AT = 'created_time';
     const UPDATED_AT = 'updated_time';
     const DELETED_AT = 'deleted_time';
+
+    //調整時間欄位使用UNIX Timestamp輸出
+    protected $casts = [
+        'created_time' => 'timestamp',
+        'updated_time' => 'timestamp',
+        'deleted_time' => 'timestamp',
+    ];
 }
